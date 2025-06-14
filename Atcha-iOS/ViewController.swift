22662b1973677b98d23d3fbcfd25b540707478ed
Riu <wodnd0418@naver.com>
2025-06-15 02:18:11 +0900

    Initial Commit

---
+++ Atcha-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Atcha-iOS
+//
+//  Created by wodnd on 6/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
