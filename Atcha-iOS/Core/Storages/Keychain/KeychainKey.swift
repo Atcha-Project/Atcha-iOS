0e3c2f636a20680f8d46514d11f561db66af0dbd
Riu <wodnd0418@naver.com>
2025-06-20 12:48:24 +0900

    [FEAT] Keychain 구현 및 kakao, alamofire, firebase 추가

---
+++ Atcha-iOS/Core/Storages/Keychain/KeychainKey.swift
@@ -0,0 +1,12 @@
+//
+//  KeychainKey.swift
+//  Atcha-iOS
+//
+//  Created by wodnd on 6/20/25.
+//
+
+import Foundation
+public extension KeychainWrapper.Key {
+    static let accessToken: Self = "accessToken"
+    static let refreshToken: Self = "refreshToken"
+}
