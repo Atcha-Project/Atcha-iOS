96380a3a9a6421c2d9784f34d3b56e1b3c2bc14f
YuGeonHui <ghyu_0906@naver.com>
2025-06-21 16:49:23 +0900

    [FEAT] 마이페이지 기본 화면 틀 구성

---
+++ Atcha-iOS/Presentation/User/MyPage/Cell/MyPageCell.swift
@@ -0,0 +1,38 @@
+//
+//  MyPageCell.swift
+//  Atcha-iOS
+//
+//  Created by geonhui Yu on 6/21/25.
+//
+
+import UIKit
+
+final class MyPageCell: UICollectionViewCell {
+    static let identifier = "MyPageCell"
+    private var atchaList: AtchaList?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .clear
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        atchaList?.removeFromSuperview()
+        atchaList = nil
+    }
+    
+    func configure(with item: MyPageItem) {
+        let list = AtchaList(title: item.title, listType: item.type)
+        contentView.addSubview(list)
+        list.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.height.equalTo(52)
+        }
+        atchaList = list
+    }
+}
---
+++ Atcha-iOS/Presentation/User/MyPage/MyPageViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MyPageViewModel.swift
+//  Atcha-iOS
+//
+//  Created by geonhui Yu on 6/21/25.
+//
+
+import Foundation
+
+final class MyPageViewModel: BaseViewModel {
+//    @Published var navigationTarget: NavigationTarget?
+}
