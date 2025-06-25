9b339ef67c39396408a7c5451035114c516959d3
Riu <wodnd0418@naver.com>
2025-06-25 16:38:56 +0900

    [REFACTOR] Onboarding UseCase 수정

--- Atcha-iOS/Presentation/Onboarding/HomeRegister/HomeRegisterViewModel.swift
+++ Atcha-iOS/Presentation/Onboarding/HomeRegister/HomeRegisterViewModel.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 final class HomeRegisterViewModel: BaseViewModel {
-
+    let onboardingUseCase: OnboardingUseCase
+    
+    init(onboardingUseCase: OnboardingUseCase) {
+        self.onboardingUseCase = onboardingUseCase
+    }
+    
+    func makeSearchLocationViewModel() -> SearchLocationViewModel {
+        return SearchLocationViewModel(onboardingUseCase: onboardingUseCase)
+    }
 }
