699957ea7e27e85122df34a82489d821b9c5d243
Riu <wodnd0418@naver.com>
2025-06-25 20:58:26 +0900

    [NETWORK] 장소 검색 API 연결

--- Atcha-iOS/Presentation/Onboarding/SearchLocation/SearchLocationViewModel.swift
+++ Atcha-iOS/Presentation/Onboarding/SearchLocation/SearchLocationViewModel.swift
@@ -10,7 +10,25 @@ import Foundation
 final class SearchLocationViewModel: BaseViewModel {
     private let onboardingUseCase: OnboardingUseCase
     
+    var onLocationsUpdated: (([Location]) -> Void)?
+    
     init(onboardingUseCase: OnboardingUseCase) {
         self.onboardingUseCase = onboardingUseCase
+    }
+    
+    @MainActor
+    func searchLocation(keyword: String, lat: Double, lon: Double) {
+        Task {
+            do {
+                let request = SearchLocationRequest(keyword: keyword, lat: lat, lon: lon)
+                let response = try await onboardingUseCase.searchLocation(request)
+                
+                DispatchQueue.main.async {
+                    self.onLocationsUpdated?(response)
+                }
+            } catch {
+                print("장소 검색 실패")
+            }
+        }
     }
 }
