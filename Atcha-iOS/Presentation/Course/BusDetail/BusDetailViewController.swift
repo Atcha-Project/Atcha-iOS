fbd3dd217ec90e7d3bbc1d73808700bff7ea6175
Riu <wodnd0418@naver.com>
2025-07-29 15:50:11 +0900

    [DESIGN] 버스 아이콘 추가

---
+++ Atcha-iOS/Presentation/Course/BusDetail/BusDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BusDetailViewController.swift
+//  Atcha-iOS
+//
+//  Created by wodnd on 7/29/25.
+//
+
+import UIKit
+import Combine
+import SnapKit
+
+class BusDetailViewController: BaseViewController<BusDetailViewModel> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
---
+++ Atcha-iOS/Presentation/Course/BusDetail/BusDetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  BusDetailViewModel.swift
+//  Atcha-iOS
+//
+//  Created by wodnd on 7/29/25.
+//
+
+import Foundation
+
+final class BusDetailViewModel: BaseViewModel {
+    
+}
