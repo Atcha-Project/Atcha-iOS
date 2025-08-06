31d34cf153197f76d183d2dba5c293738ef1950c
YuGeonHui <ghyu_0906@naver.com>
2025-08-06 10:39:27 +0900

    [FEAT] 알림 이후 View 개발 완료

--- Atcha-iOS/Presentation/Location/View/LastTrainRealTimeBottomView.swift
+++ Atcha-iOS/Presentation/Location/View/LastTrainRealTimeBottomView.swift
@@ -18,6 +18,12 @@ final class LastTrainRealTimeBottomView: UIView {
     
     let actionPublisher = PassthroughSubject<Action, Never>()
     
+    private let titleView: UIView = UIView()
+    private let iconImageView: UIImageView = UIImageView()
+    private let trainInfoLabel: UILabel = UILabel()
+    private let remainStationLabel: UILabel = UILabel()
+    private let reloadImageView: UIImageView = UIImageView()
+    
     private let timeView: UIView = UIView()
     private let minuteLabel: UILabel = UILabel()
     private let minuteTimeLabel: UILabel = UILabel()
@@ -40,7 +46,6 @@ final class LastTrainRealTimeBottomView: UIView {
     
     private let locationLabel: UILabel = UILabel()
     
-    private let reloadImageView: UIImageView = UIImageView()
     private lazy var buttonStackView: UIStackView = {
         let stackView: UIStackView = UIStackView(arrangedSubviews: [exitButton, detailRoadMapButton])
         stackView.spacing = 12
@@ -72,14 +77,139 @@ final class LastTrainRealTimeBottomView: UIView {
     }
     
     private func setupUI() {
+        backgroundColor = .gray950
+        layer.cornerRadius = 20
+        titleView.addSubViews(iconImageView,
+                              trainInfoLabel,
+                              remainStationLabel,
+                              reloadImageView)
+        timeView.addSubViews(minuteLabel,
+                             minuteTimeLabel,
+                             secondLabel,
+                             secondTimeLabel,
+                             alreadySoonLabel)
+        addSubViews(titleView,
+                    timeView,
+                    locationLabel,
+                    buttonStackView)
+        
+        exitButton.setContentHuggingPriority(.required, for: .horizontal)
+        exitButton.setContentCompressionResistancePriority(.required, for: .horizontal)
+        
+        detailRoadMapButton.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        detailRoadMapButton.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        
+        reloadImageView.image = UIImage.refreshOutlined
+        reloadImageView.contentMode = .scaleAspectFit
+        reloadImageView.tintColor = .white
+        
+        // MARK: Test
+        iconImageView.image = UIImage.route16PxBus
+        iconImageView.tintColor = .green
+        trainInfoLabel.attributedText = AtchaFont.B4_R_15("강남역", color: .white)
+        remainStationLabel.attributedText = AtchaFont.B4_R_15("6정류장 전", color: .white)
+        
+        minuteLabel.attributedText = AtchaFont.B1_R_17("분", color: .widearea)
+        secondLabel.attributedText = AtchaFont.B1_R_17("초", color: .widearea)
+        minuteTimeLabel.attributedText = AtchaFont.D2_EB_48("\(07)", color: .widearea)
+        secondTimeLabel.attributedText = AtchaFont.D2_EB_48("\(05)", color: .widearea)
     }
     
     private func setupAutoLayout() {
+        titleView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(16)
+            make.trailing.equalToSuperview().inset(20)
+            make.top.equalToSuperview().inset(24)
+            make.height.equalTo(28)
+        }
         
+        iconImageView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.size.equalTo(16)
+        }
+        
+        trainInfoLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(iconImageView.snp.trailing).offset(4)
+            make.height.equalTo(20)
+        }
+        
+        remainStationLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(trainInfoLabel.snp.trailing).offset(6)
+            make.height.equalTo(20)
+        }
+        
+        reloadImageView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.size.equalTo(28)
+        }
+        
+        timeView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(16)
+            make.trailing.equalToSuperview().inset(20)
+            make.top.equalTo(titleView.snp.bottom).inset(-4)
+            make.height.equalTo(66)
+        }
+        
+        minuteTimeLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+        
+        minuteLabel.snp.makeConstraints { make in
+            make.leading.equalTo(minuteTimeLabel.snp.trailing).offset(4)
+            make.centerY.equalTo(minuteTimeLabel.snp.bottom).offset(-20)
+        }
+        
+        secondTimeLabel.snp.makeConstraints { make in
+            make.leading.equalTo(minuteLabel.snp.trailing).offset(12)
+            make.centerY.equalToSuperview()
+        }
+        
+        secondLabel.snp.makeConstraints { make in
+            make.leading.equalTo(secondTimeLabel.snp.trailing).offset(4)
+            make.centerY.equalTo(secondTimeLabel.snp.bottom).offset(-20)
+        }
+        
+        locationLabel.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.height.equalTo(20)
+            make.top.equalTo(timeView.snp.bottom).offset(16)
+        }
+        
+        buttonStackView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview().inset(40)
+            make.top.equalTo(locationLabel.snp.bottom).offset(16)
+            make.height.equalTo(88)
+        }
     }
     
     private func setupActions() {
+        exitButton.addTarget(self, action: #selector(handleExitTapped), for: .touchUpInside)
+        detailRoadMapButton.addTarget(self, action: #selector(handleDetailRoadTapped), for: .touchUpInside)
+        reloadImageView.isUserInteractionEnabled = true
+        reloadImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleReloadTapped)))
+        locationLabel.isUserInteractionEnabled = true
+        locationLabel.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleLocationTapped)))
+    }
+}
+
+// MARK: Binding Leg Info
+extension LastTrainRealTimeBottomView {
+    func setupLegInfo(info: LegInfo) {
         
+    }
+}
+
+extension LastTrainRealTimeBottomView {
+    func setupLoaction(location: String?) {
+        guard let location else { return }
+        let title: String = "\(location) -> 우리집"
+        locationLabel.attributedText = AtchaFont.B4_R_15(title, color: .gray300)
     }
 }
 
