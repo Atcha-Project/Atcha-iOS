e0417dd469436c30f535b8f12c7684d40fb80b20
EumJaeWoong <wodnd0418@naver.com>
2025-11-19 08:52:44 +0900

    [bugfix] 현위치 버튼 활성화 및 지도 center 보정

--- Atcha-iOS/Presentation/Location/View/TMapContainerView.swift
+++ Atcha-iOS/Presentation/Location/View/TMapContainerView.swift
@@ -40,6 +40,7 @@ final class TMapContainerView: UIView {
     
     func setupCenter(location: CLLocationCoordinate2D) {
         tMapWrapper.mapView.setCenter(location)
+        tMapWrapper.mapView.setZoom(18)
     }
     
     func updateUserMarker(location: CLLocationCoordinate2D) {
