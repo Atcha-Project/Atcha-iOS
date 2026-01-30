ee14c3504483f985b11a034066362eda4378933a
JaeWoong Eum <wodnd0418@gmail.com>
2026-01-31 03:40:04 +0900

    [REFACTOR] R13 폰트 추가 및 적용

--- Atcha-iOS/DesignSource/AtchaFont/AtchaFont.swift
+++ Atcha-iOS/DesignSource/AtchaFont/AtchaFont.swift
@@ -136,6 +136,11 @@ enum AtchaFont {
         styled(.Medium, size: 9, lineHeight: lineHeight, text: text, color: color, alignment: alignment)
     }
     
+    // B8_R_13
+    static func R_13(lineHeight: CGFloat = 15, _ text: String, color: UIColor = .label, alignment: NSTextAlignment = .left) -> NSAttributedString {
+        styled(.Regular, size: 13, lineHeight: lineHeight, text: text, color: color, alignment: alignment)
+    }
+    
     // MARK: - 사용 예시
     //
     // UILabel:
--- Atcha-iOS/Presentation/Course/CourseSearch/CourseCell.swift
+++ Atcha-iOS/Presentation/Course/CourseSearch/CourseCell.swift
@@ -122,7 +122,7 @@ final class CourseCell: UICollectionViewCell {
         }
         
         if let departureTime = course.departureDateTime {
-            departureTimeLabel.attributedText = AtchaFont.B7_M_13(lineHeight: 15, "\(departureTime.convertedToHourMinute)", color: AtchaColor.white)
+            departureTimeLabel.attributedText = AtchaFont.R_13(lineHeight: 15, "\(departureTime.convertedToHourMinute)", color: AtchaColor.white)
             departureTimeLabel.layer.cornerRadius = 8
             departureTimeLabel.clipsToBounds = true
             departureTimeLabel.backgroundColor = AtchaColor.gray930
@@ -130,7 +130,7 @@ final class CourseCell: UICollectionViewCell {
             departureTimeLabel.layer.borderColor = AtchaColor.gray500.cgColor
         }
         
-        departureLabel.attributedText = AtchaFont.B7_M_13("에 자리에서 출발", color: AtchaColor.gray200)
+        departureLabel.attributedText = AtchaFont.R_13("에 자리에서 출발", color: AtchaColor.gray200)
         
         progressView.configure(infos: course.toLegTrafficInfos())
         courseStepsStackView.configure(legs: course.legs)
