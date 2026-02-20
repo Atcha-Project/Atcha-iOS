b373e54ea8096cc932d3b6dc73b07ab6504eac58
JaeWoong Eum <wodnd0418@gmail.com>
2026-02-20 18:45:05 +0900

    [REFACTOR] 스플래쉬 배경 수정 및 AppConfig 적용

--- Atcha-iOS/App/AppDelegate.swift
+++ Atcha-iOS/App/AppDelegate.swift
@@ -19,14 +19,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         UNUserNotificationCenter.current().delegate = self
         
         // MARK: - Kakao
-        print("Bundle.main.kakaoInitKey : \(Bundle.main.kakaoApiKey)")
-        KakaoSDK.initSDK(appKey: Bundle.main.kakaoApiKey)
+        KakaoSDK.initSDK(appKey: AppConfig.kakaoApiKey)
         
         // MARK: - Firebase
         FirebaseApp.configure()
         Messaging.messaging().delegate = self
         application.registerForRemoteNotifications()
-        
+
         AmplitudeManager.shared.reset()
         
         let savedId = UserDefaultsWrapper.shared.integer(forKey: UserDefaultsWrapper.Key.userId.rawValue)
